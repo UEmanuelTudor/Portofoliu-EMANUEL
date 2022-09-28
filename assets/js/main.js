7809263176369ebabed12fff820668ea556fb19f
UEmanuelT <uifalean_e@yahoo.com>
2022-09-28 22:15:55 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,9 +92,37 @@ modalCloses.forEach((modalClose) =>{
 })
 
 /*============================= PORFOLIO SWIPER ==========================*/
+let swiperPortfolio = new Swiper( '.portfolio__container', {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 
 /*============================= TESTIMONIAL ===============================*/
+let swiperTestimonial = new Swiper( '.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2, 
+        }
+    }
+  });
 
 
 /*============================= SCROLL SECTION ACTIVE LINK ==================*/
